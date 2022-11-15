d9df5e2b57b8701c1ead8001ef050af428d72ec6
alisson-ap <alisson08silva@gmail.com>
2022-11-15 12:01:56 -0300

    Update service

--- Tcoder-42-frontend/src/app/courses/services/courses.service.ts
+++ Tcoder-42-frontend/src/app/courses/services/courses.service.ts
@@ -22,12 +22,25 @@ export class CoursesService {
       );
   }
 
-  save(record: Partial<Course>){
+  save(record: Partial<Course>) {
+    if (record._id) {
+      return this.update(record);
+    } else {
+      return this.create(record);
+    }
+  }
+
+  loadById(id: string) {
+    return this.httpClient.get<Course>(`${this.API}/${id}`);
+  }
+
+  private create(record: Partial<Course>,) {
     return this.httpClient.post<Course>(this.API, record).pipe(first());
   }
 
-  loadById(id: string){
-   return this.httpClient.get<Course>(`${this.API}/${id}`);
+  private update(record: Partial<Course>) {
+    return this.httpClient.put<Course>(`${this.API}/${record._id}`, record).pipe(first());
+
   }
 
 }
